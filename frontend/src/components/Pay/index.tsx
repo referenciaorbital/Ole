8c441af807456bac9e38465904adf5c39597646f
referenciaorbital <referenciaorbital@gmail.com>
2025-01-24 22:24:53 -0500

    Update index.tsx

--- frontend/src/components/Pay/index.tsx
+++ frontend/src/components/Pay/index.tsx
@@ -20,7 +20,7 @@ const sendPayment = async () => {
 
     const payload: PayCommandInput = {
       reference: id,
-      to: "0x0c892815f0B058E69987920A23FBb33c834289cf", // Test address
+      to: "0x512e4a7dda6b13f917d89fa782bdd7666dab1599", // Test address
       tokens: [
         {
           symbol: Tokens.WLD,
